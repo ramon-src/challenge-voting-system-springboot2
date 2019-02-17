7a19586b43c291da84407ede5737b6d03ece2ac4
rschmidt <ramonroc@gmail.com>
2019-02-17 17:32:58 -0300

    GuidelineRepository now can Associate Users to Guidelines

--- src/main/java/com/java/challenge/voting_system/infra/repository/GuidelineRepository.java
+++ src/main/java/com/java/challenge/voting_system/infra/repository/GuidelineRepository.java
@@ -1,26 +1,52 @@
 package com.java.challenge.voting_system.infra.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.java.challenge.voting_system.guideline.Guideline;
+import com.java.challenge.voting_system.user.User;
 
 public class GuidelineRepository {
 	
 	private List<Guideline> guidelines;
+	private Map<Guideline, List<User>> associatedUsers;
 	
 	public GuidelineRepository() {
 		guidelines = new ArrayList<Guideline>();
+		associatedUsers = new HashMap<Guideline, List<User>>();
 	}
 	
 	public Guideline get(Guideline guideline) {
 		return guidelines.stream().filter(gl -> gl.getId() == guideline.getId()).findFirst().get();
 	}
 
+	public Boolean exists(Guideline guideline) {
+		return guidelines.stream().filter(gl -> gl.getId() == guideline.getId()).findFirst().isPresent();
+	}
+	
+	public void associate(User user, Guideline guideline) throws Exception {
+		Boolean exists = exists(guideline);
+		if (!exists)
+			throw new Exception("A pauta não existe");
+		
+		associatedUsersFrom(guideline).add(user);
+	}
+
+	protected List<User> associatedUsersFrom(Guideline guideline) {
+		List<User> associateds = associatedUsers.get(guideline);
+		if (associateds == null) {
+			ArrayList<User> users = new ArrayList<User>();
+			associatedUsers.put(guideline, users);
+			associateds = associatedUsers.get(guideline);
+		}
+		return associateds;
+	}
+	
 	public void create(Guideline guideline) {
 		guidelines.add(guideline);
 	}
-	
 
 	public List<Guideline> getGuidelines() {
 		return guidelines;
@@ -29,4 +55,13 @@ public class GuidelineRepository {
 	public void setGuidelines(List<Guideline> guidelines) {
 		this.guidelines = guidelines;
 	}
+
+	public Map<Guideline, List<User>> getAssociatedUsers() {
+		return associatedUsers;
+	}
+
+	public void setAssociatedUsers(Map<Guideline, List<User>> associatedUsers) {
+		this.associatedUsers = associatedUsers;
+	}
+	
 }
--- src/test/java/com/java/challenge/voting_system/infra/repository/GuidelineRepositoryTest.java
+++ src/test/java/com/java/challenge/voting_system/infra/repository/GuidelineRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.java.challenge.voting_system.infra.repository;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,20 +17,23 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import com.java.challenge.voting_system.guideline.Guideline;
+import com.java.challenge.voting_system.user.User;
 
 public class GuidelineRepositoryTest {
 
 	@InjectMocks
-	private  GuidelineRepository guidelineRepository;
+	private GuidelineRepository guidelineRepository;
 
 	@Mock
 	private Guideline guideline;
+	@Mock
+	private User user;
 
 	@Before
 	public void setup() {
 		initMocks(this);
 	}
-
+	
 	@Test
 	public void should_getTheGuideline_when_matchId() {
 		guideline.setId(10L);
@@ -42,6 +50,53 @@ public class GuidelineRepositoryTest {
 		guidelineRepository.create(guideline);
 		
 		assertEquals(1, guidelineRepository.getGuidelines().size());
+	}
+
+	@Test(expected = Exception.class)
+	public void should_notAssociateUsers_when_guidelineNotFound() throws Exception {
+		guideline.setId(10L);
+		
+		guidelineRepository.associate(user, guideline);
+	}
+	
+	@Test
+	public void should_existTheGuideline() {
+		guideline.setId(10L);
+		List<Guideline> guidelines = Arrays.asList(guideline);
+		guidelineRepository.setGuidelines(guidelines);
+		
+		assertTrue(guidelineRepository.exists(guideline));
+	}
+	
+	@Test
+	public void should_not_existTheGuideline() {		
+		assertFalse(guidelineRepository.exists(guideline));
+	}
+	
+	@Test
+	public void should_associateUsersToGuideline_evenWhen_guidelineIsNotAssociatedYet() throws Exception {
+		guideline.setId(10L);
+		List<Guideline> guidelines = Arrays.asList(guideline);
+		guidelineRepository.setGuidelines(guidelines);
+		
+		guidelineRepository.associate(user, guideline);
+		
+		assertEquals(1, guidelineRepository.getAssociatedUsers().size());
+	}
+
+	@Test
+	public void should_associateUsersToGuideline_when_guidelineWasAssociated() throws Exception {
+		guideline.setId(10L);
+		List<Guideline> guidelines = Arrays.asList(guideline);
+		guidelineRepository.setGuidelines(guidelines);
+		
+		Map<Guideline, List<User>> associatedUsers = new HashMap<Guideline, List<User>>();
+		associatedUsers.put(guideline, new ArrayList<User>());
+		guidelineRepository.setAssociatedUsers(associatedUsers);
+		
+		guidelineRepository.associate(user, guideline);
+		
+		assertEquals(1, guidelineRepository.getAssociatedUsers().size());
 	}
 
 }
